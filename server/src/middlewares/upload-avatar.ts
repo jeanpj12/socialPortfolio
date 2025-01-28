65ddc1c39c06e714f67042f4a5e6300983c78a5e
jeanpj12 <jeanpj12@gmail.com>
2025-01-28 18:39:40 -0300

    Adjust path upload

--- server/src/middlewares/upload-avatar.ts
+++ server/src/middlewares/upload-avatar.ts
@@ -3,7 +3,7 @@ import multer from 'multer';
 import path from 'path';
 import fs from 'fs';
 
-const uploadDir = path.resolve(__dirname, '..', '..', 'uploads', 'avatar');
+const uploadDir = path.resolve(__dirname, '..', 'uploads', 'avatar');
 
 if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir, { recursive: true });
--- server/src/middlewares/upload-image-post.ts
+++ server/src/middlewares/upload-image-post.ts
@@ -3,7 +3,7 @@ import multer from 'multer';
 import path from 'path';
 import fs from 'fs';
 
-const uploadDir = path.resolve(__dirname, '..', '..', 'uploads', 'image-post');
+const uploadDir = path.resolve(__dirname, '..', 'uploads', 'image-post');
 
 if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir, { recursive: true });
