d4b4fcccc477d0aac5bd0227ff70741241de39ef
Jean <jeanpj12@gmail.com>
2025-02-18 12:50:34 -0300

    side Ads

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,66 +1,72 @@
-import { SideProfile } from './components/SideProfile'
-import styles from './App.module.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { routes } from './routes/Routes'
-import { Modal } from './modal'
-import { useEffect, useState } from 'react'
-import { FormLogin } from './components/FormLogin'
-import { FormSignUp } from './components/FormSignUp'
-import { Loading } from './components/Loading'
-import ReactGA from 'react-ga4';
+import { SideProfile } from "./components/SideProfile";
+import styles from "./App.module.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { routes } from "./routes/Routes";
+import { Modal } from "./modal";
+import { useEffect, useState } from "react";
+import { FormLogin } from "./components/FormLogin";
+import { FormSignUp } from "./components/FormSignUp";
+import { Loading } from "./components/Loading";
+import ReactGA from "react-ga4";
+import { SideAds } from "./components/SideAds";
 
 function App() {
-    const analyticsCode = process.env.GTAG_ID
-    console.log(analyticsCode)
-    const [loginModal, setLoginModal] = useState(false)
-    const [signUpModal, setSignUpModal] = useState(false)
-    const [loading, setLoading] = useState(false)
+  const analyticsCode = process.env.GTAG_ID;
+  console.log(analyticsCode);
+  const [loginModal, setLoginModal] = useState(false);
+  const [signUpModal, setSignUpModal] = useState(false);
+  const [loading, setLoading] = useState(false);
 
+  useEffect(() => {
+    if (analyticsCode) ReactGA.initialize(analyticsCode);
+    ReactGA.send({
+      hitType: "pageview",
+      page: window.location.pathname,
+      title: "App.jsx",
+    });
+  }, []);
 
-    useEffect(() => {
-        if (analyticsCode) ReactGA.initialize(analyticsCode)
-        ReactGA.send({ hitType: "pageview", page: window.location.pathname, title: "App.jsx" });
+  return (
+    <div>
+      {loading && <Loading />}
+      {loginModal && (
+        <Modal title="Login" closeModal={setLoginModal}>
+          <FormLogin setLoading={setLoading} />
+        </Modal>
+      )}
 
-    }, [])
+      {signUpModal && (
+        <Modal title="Login" closeModal={setSignUpModal}>
+          <FormSignUp setLoading={setLoading} />
+        </Modal>
+      )}
 
-    return (
-        <div>
-            {loading && <Loading />}
-            {
-                loginModal && <Modal
-                    title='Login'
-                    closeModal={setLoginModal}>
-                    <FormLogin setLoading={setLoading} />
-                </Modal>
-            }
-
-            {
-                signUpModal && <Modal
-                    title='Login'
-                    closeModal={setSignUpModal}>
-                    <FormSignUp setLoading={setLoading} />
-                </Modal>
-            }
-
-            <div className={styles.container}>
-                <Router>
-                    <SideProfile
-                        openLoginModal={setLoginModal}
-                        openSingUpModal={setSignUpModal}
-                        setLoading={setLoading}
-                    />
-                    <div className={styles.content}>
-                        <Routes>
-                            {routes.map((route) => {
-                                const Component = route.component
-                                return <Route key={route.path} path={route.path} element={<Component />} />
-                            })}
-                        </Routes>
-                    </div>
-                </Router>
-            </div>
-        </div>
-    )
+      <div className={styles.container}>
+        <Router>
+          <SideProfile
+            openLoginModal={setLoginModal}
+            openSingUpModal={setSignUpModal}
+            setLoading={setLoading}
+          />
+          <div className={styles.content}>
+            <Routes>
+              {routes.map((route) => {
+                const Component = route.component;
+                return (
+                  <Route
+                    key={route.path}
+                    path={route.path}
+                    element={<Component />}
+                  />
+                );
+              })}
+            </Routes>
+          </div>
+        </Router>
+        <SideAds />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- client/src/components/SideAds/index.tsx
+++ client/src/components/SideAds/index.tsx
@@ -1,10 +1,8 @@
 import styles from './styles.module.css'
+import Ad01 from '../../assets/ads01.webp'
 
 export function SideAds(){
     return <div className={styles.sideBar}>
-        <div className={styles.ads}><span>Anunciante</span></div>
-        <div className={styles.ads}><span>Anunciante</span></div>
-        <div className={styles.ads}><span>Anunciante</span></div>
-        <div className={styles.ads}><span>Anunciante</span></div>
+        <a href="https://hostinger.com.br?REFERRALCODE=C4GJEANJR853" target='_blank'><img src={Ad01}/></a>
     </div>
 }
